b7fe3e7869063bc1601b9c509d22a3548cf7a114
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2023-07-21 16:52:56 +0300

    comment updated by Haydar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a demo project which is done by Aykut.
+          This is a demo project which is done by Haydar.
         </p>
         <a
           className="App-link"
